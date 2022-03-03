9a4f461b1eb62d9a3041a2a25e491027c6d4c78a
yang <gluestuck@naver.com>
2022-03-03 22:30:58 +0900

    -로그인 화면 생성 -프로필 화면 생성

---
+++ prepare/front/components/LoginForm.js
@@ -0,0 +1,52 @@
+import {Button, Form, Input} from 'antd';
+import {useCallback, useMemo, useState} from 'react';
+import Link from 'next/link';
+import styled from "styled-components";
+
+const ButtonWrapper = styled.div`
+  marginTop: 10px;`
+
+const FormWrapper = styled(Form)`
+    padding: 10px;
+`
+
+const LoginForm = ({setIsLoggedIn}) => {
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onChangeId = useCallback((e) => {
+        setId(e.target.value);
+    }, []);
+
+    const onChangePassword = useCallback((e) => {
+        setPassword(e.target.value);
+    }, []);
+
+    const style = useMemo(() => ({marginTop: 10}), []);
+
+    const onSubmitForm = useCallback(()=>{
+        console.log(id,password);
+        setIsLoggedIn(true);
+    },[id,password]);
+
+    return (
+        <FormWrapper onFinish={onSubmitForm}>
+            <div>
+                <label htmlFor="user-id">아이디</label>
+                <br/>
+                <Input name="user-id" value={id} onChange={onChangeId} required/>
+            </div>
+            <div>
+                <label htmlFor="user-id">비밀번호</label>
+                <br/>
+                <Input name="user-password" type="password" value={password} onChange={onChangePassword} required/>
+            </div>
+            <ButtonWrapper>
+                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                <Link href="/signup"><a>회원가입</a></Link>
+            </ButtonWrapper>
+        </FormWrapper>
+    );
+}
+
+export default LoginForm;
---
+++ prepare/front/pages/index.js
@@ -0,0 +1,11 @@
+//next는 pages 폴더 안에 있는 파일들을 자동으로
+
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+    return (
+        <AppLayout><div>Hello, Next!</div></AppLayout>
+    );
+}
+
+export default Home;
