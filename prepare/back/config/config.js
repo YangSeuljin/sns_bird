7f9b89480a49350a66bbcadf1bdb79fe0dea6000
yang <gluestuck@naver.com>
2022-04-03 21:11:27 +0900

    - db 비밀번호 변경

--- prepare/back/config/config.js
+++ prepare/back/config/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = {
   "development": {
     "username": "root",
-    "password": "842696",
+    "password": "Openmate1!",
     "database": "react-nodebird",
     "host": "127.0.0.1",
     "port": "3306",
@@ -12,14 +12,14 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": "842696",
+    "password": "Openmate1!",
     "database": "react-nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": "842696",
+    "password": "Openmate1!",
     "database": "react-nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
